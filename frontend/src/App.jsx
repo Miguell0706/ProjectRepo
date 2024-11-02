c08cb31e707f5f8eae75ee8da0ca097dff527392
Miguel <Miguellozano3757@gmail.com>
2024-11-02 00:30:10 -0700

    trying to deploy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import GrowthPage from "./pages/GrowthPage";
-import HomePage from "./pages/HomePage";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<GrowthPage />} />
-        <Route path="/create" element={<GrowthPage />} />
       </Routes>
     </>
   );

b8bb85c91f4c878beeb87c4fffcedfb9e9019628
Miguel <Miguellozano3757@gmail.com>
2024-11-18 21:47:53 -0700

    made discover now button need to layout now

--- frontend/src/components/HomePageComponents/Hero.jsx
+++ frontend/src/components/HomePageComponents/Hero.jsx
@@ -5,12 +5,18 @@ const Hero = () => {
   return (
     <section className="hero">
       <div className="hero__content">
-        <h1>Welcome to Our Website</h1>
-        <p>
-          Your journey starts here. Explore our services and discover what we
-          can offer you.
-        </p>
-        <button className="hero__button">Learn More</button>
+        <h1>Welcome to YourYouthBook</h1>
+        <p>The ultimate gift for your child</p>
+        <button className="hero__button">
+          Discover now
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            className="hero__arrow"
+          >
+            <path d="M16.671,9.998 L12.997,9.998 L16.462,6.000 L5.000,6.000 L5.000,4.000 L16.462,4.000 L12.997,0.002 L16.671,0.002 L21.003,5.000 L16.671,9.998 ZM17.000,5.379 L17.328,5.000 L17.000,4.621 L17.000,5.379 ZM-0.000,4.000 L3.000,4.000 L3.000,6.000 L-0.000,6.000 L-0.000,4.000 Z"></path>
+          </svg>
+        </button>
       </div>
     </section>
   );
